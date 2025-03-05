bc124e464e52e99c970ada222c74a8293ed0028f
oya0425 <008tessho@gmail.com>
2025-03-05 16:14:31 +0900

    減速、加速の実装（仮）

--- Assets/WorkSpace/Oya/Player_oya/Player_auto_run_oya.cs
+++ Assets/WorkSpace/Oya/Player_oya/Player_auto_run_oya.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player_auto_run_oya : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float accelMultiplier = 2f; // 加速時の倍率
+    [SerializeField] float accelDuration = 2f; // 加速する時間（秒）
+
+    private bool isAccelerating = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +20,34 @@ public class Player_auto_run_oya : MonoBehaviour
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacles")
+        {
+            ChangeSpeed(0.5f);
+        }
+        if (collision.gameObject.tag == "Item")
+        {
+            ChangeSpeed(20);
+        }
 
+    }
+    private void ChangeSpeed(float accelMultiplier)
+    {
+        if (!isAccelerating) // すでに加速中でなければ開始
+        {
+            StartCoroutine(Accelerate(accelMultiplier));
+        }
+    }
+    private IEnumerator Accelerate(float accelMultiplier)
+    {
+        isAccelerating = true;
+        float originalSpeed = speed;
+        speed *= accelMultiplier; // 加速
+
+        yield return new WaitForSeconds(accelDuration); // 指定時間待つ
+
+        speed = originalSpeed; // 元の速度に戻す
+        isAccelerating = false;
+    }
 }
