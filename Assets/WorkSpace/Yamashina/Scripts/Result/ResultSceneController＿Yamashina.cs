571de2858a36c54e5012d9b5cb90a5d1639e5d8d
maho0628 <maho06282001@gmail.com>
2025-03-06 15:06:37 +0900

    リザルト画面。投げる回数以外の処理終了

--- Assets/WorkSpace/Yamashina/Scripts/Result/ResultSceneController＿Yamashina.cs
+++ Assets/WorkSpace/Yamashina/Scripts/Result/ResultSceneController＿Yamashina.cs
@@ -6,36 +6,55 @@ using UnityEngine.UI;
 public class ResultSceneController＿Yamashina : MonoBehaviour
 {
     [SerializeField, Header("リザルトイメージを表示させる場所")] private Image resultImage;
-    [SerializeField] private Text elapsedTimeText;
-    [SerializeField] private Text throwCountText;
+    [SerializeField, Header("リザルトの文字のイメージを表示させる場所")] private Image resultTextImage;
+
+    [SerializeField, Header("ゲームリスタートボタン")] private Button restartButton;
+    [SerializeField, Header("ゲームリスタートボタン")] private Button titleButton;
+
+    [SerializeField, Header("経過時間のテキスト表示場所")] private Text elapsedTimeText;
+    [SerializeField, Header("投げた回数のテキスト場所")] private Text throwCountText;
 
     [SerializeField, Header("プレイヤーが勝ったときのリザルト画像")] private Sprite VictoriousSprite; // 弾に当たった場合の画像
     [SerializeField, Header("プレイヤーが負けたときのリザルト画像")] private Sprite defeatedSprite;  // 逃げ切った場合の画像
+    [SerializeField, Header("プレイヤーが勝ったときのリザルト画像")] private Sprite VictoriousTextSprite; // 弾に当たった場合の画像
+    [SerializeField, Header("プレイヤーが負けたときのリザルト画像")] private Sprite defeatedTextSprite;  // 逃げ切った場合の画像
     // Start is called before the first frame update
     void Start()
     {
-        elapsedTimeText.text = "";
-        throwCountText.text = "";
+        InitializeReferences();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeReferences()
     {
-        elapsedTimeText.text = Mathf.FloorToInt(Time.time).ToString();
+        int totalSeconds = Mathf.FloorToInt(Time.time); // 経過秒数を整数化
+        int minutes = totalSeconds / 60;  // 分を計算
+        int seconds = totalSeconds % 60;  // 余った秒を計算
+        elapsedTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         throwCountText.text = "";
+        restartButton.onClick.AddListener(() => DestroyGameManager());
+        titleButton.onClick.AddListener(() => DestroyGameManager());
+
         // GameManager の状態によって画像を変更
         switch (GameManager_Yamashina.GetState())
         {
             case GameManager_Yamashina.EnemyState.Defeated:
                 resultImage.sprite = VictoriousSprite;
+                resultTextImage.sprite = VictoriousTextSprite;
 
                 break;
             case GameManager_Yamashina.EnemyState.Escaped:
                 resultImage.sprite = defeatedSprite;
+                resultTextImage.sprite = defeatedTextSprite;
                 break;
             default:
                 Debug.LogWarning("GameManagerのenemyStateが未設定です");
                 break;
         }
+    }
+
+    void DestroyGameManager()
+    {
+        Destroy(GameManager_Yamashina.instance.gameObject);
     }
 }
