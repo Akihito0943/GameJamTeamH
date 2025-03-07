ec74e70b3f6afa152912ff97fb0d06e5819a9e0a
Akihito0943 <vnKumagae094385@gmail.com>
2025-03-07 18:13:34 +0900

    様々な調整

---
+++ Assets/WorkSpace/Kumagae/Scripts/PlayerFallOut_Kumagae.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFallOut_Kumagae : MonoBehaviour
+{
+    void Update()
+    {
+        // Y座標が-15以下になったらゲーム終了
+        if(transform.position.y < -15)
+        {
+            Debug.Log("落ちた");
+        }
+    }
+}
---
+++ Assets/WorkSpace/Kumagae/Scripts/SceneChange_Kumagae.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange_Kumagae : MonoBehaviour
+{
+    [SerializeField, Header("Fade用のキャンバス")]
+    Canvas canvasFade;
+
+    [SerializeField, Header("Fade用のキャンバスグループ")]
+    CanvasGroup cgFade;
+
+    [SerializeField, Header("フェードにかかる時間")]
+    float fadeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private IEnumerator ThrowAfterCoolTimer()
+    {
+        yield return new WaitForSeconds(fadeTime);
+    }
+
+}
